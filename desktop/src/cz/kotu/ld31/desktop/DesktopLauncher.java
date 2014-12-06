cca479bee802ee8b6356fe3f49707e30e5915679
Tomas Kotula <tomasrkotula@gmail.com>
2014-12-06 12:31:32 +0100

    created project using libgdx project generator

---
+++ desktop/src/cz/kotu/ld31/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package cz.kotu.ld31.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import cz.kotu.ld31.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
---
+++ html/src/cz/kotu/ld31/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package cz.kotu.ld31.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import cz.kotu.ld31.MyGdxGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new MyGdxGame();
+        }
+}
