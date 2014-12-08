3d7a6bff6965f6ff27354590d3935ff1e6a74b61
Tomas Kotula <tomasrkotula@gmail.com>
2014-12-08 03:08:55 +0100

    branding - icon + name

--- desktop/src/cz/kotu/ld31/desktop/DesktopLauncher.java
+++ desktop/src/cz/kotu/ld31/desktop/DesktopLauncher.java
@@ -1,12 +1,15 @@
 package cz.kotu.ld31.desktop;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import cz.kotu.ld31.MyGdxGame;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MyGdxGame(), config);
-	}
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = "Sliding StOnes :: Tomas Kotula (kotucz) :: Ludum Dare 31";
+        config.addIcon("icon32.png", Files.FileType.Internal);
+        new LwjglApplication(new MyGdxGame(), config);
+    }
 }
